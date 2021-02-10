9ceeaed75533ce8a509255ac9418882070c2cb2d
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-10 18:07:43 +0000

    added comments and changed to single quotations

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+// when the button named click me is clicked, the html calls the function, textButton, and it changes the text
 function textButton() {
-  document.getElementById("sentence").innerHTML = "after button"
+  document.getElementById("sentence").innerHTML = 'after button'
 }
 
+// when the button, click to see the kitten is clicked, the html calls the function, kittenButton, and the javascript code changes the image
 function kittenButton() {
-  document.getElementById("image").src = "./images/kitten.jfif"
+  document.getElementById("image").src = './images/kitten.jfif'
 }
 
+// when the button, change background is clicked, the html calls the function, backgroundButton, and the javascript code that is in the function, backgroundButton, changes the background color
 function backgroundButton() {
-  document.getElementById("background").style.backgroundColor = "powderblue"
+  document.getElementById("background").style.backgroundColor = 'powderblue'
 }
 
+// when the button, make the date appear, is clicked, the html calls the function, time, from the javascript and the date appears
 function time() {
   document.getElementById("appear").innerHTML=Date()
 }
