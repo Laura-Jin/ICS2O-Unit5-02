2853716c2b8d3672c276c31bf5b54e373c842cea
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-10 17:55:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function textButton() {
+  document.getElementById("sentence").innerHTML = "after button"
+}
+
+function kittenButton() {
+  document.getElementById("image").src = "./images/kitten.jfif"
+}
+
+function backgroundButton() {
+  document.getElementById("background").style.backgroundColor = "powderblue"
+}
+
+function time() {
+  document.getElementById("appear").innerHTML=Date()
+}
